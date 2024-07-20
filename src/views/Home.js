a5dce996c53a68a46fca8b3964e22d763b030baa
Joao Paulo Cordio <joao.cordio@gmail.com>
2024-07-20 10:15:13 +0100

    Adding 'our family' line on home page

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,7 +1,10 @@
 import MyCarousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import logo from "../assets/images/logo/logo_color.webp";
+import Logo from "../assets/images/logo/logo_color.webp";
+import OurFamily1 from "../assets/images/cards/CindyFace.webp";
+import OurFamily2 from "../assets/images/cards/BritaFace.webp";
+import OurFamily3 from "../assets/images/cards/OllieFace.webp";
 import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
@@ -15,7 +18,7 @@ function Home() {
         <div class="container" id="contentAboutUs">
             <div class="row mt-5">
                 <div class="col-12 col-md-5">
-                    <img src={logo} class="mx-auto d-block logo" alt="logo" />
+                    <img src={Logo} class="mx-auto d-block logo" alt="logo" />
                 </div>
                 <div class="col-12 col-md-7">
                     <div class="card mt-5">
@@ -32,6 +35,49 @@ function Home() {
                     </div>
                 </div>
             </div> 
+        </div>
+
+        {/* Container for "Our Family" */}
+        <div class="container" id="contentOurFamily">
+            <div class="row mt-5">
+                <div class="col-12 text-center mb-3">
+                    <h1>Our Family</h1>
+                </div>
+                <div class="card-group">
+                    <div class="col-12 col-md-4 mb-2">
+                        <div class="card">
+                            <img class="card-img-top dogFace" src={OurFamily1} alt="Card image cap" />
+                            <div class="card-body">
+                                <h5 class="card-title">Cindy</h5>
+                                <p class="card-text">Meet Cindy, the remarkable Alpha female in our kennel. She holds Irish Junior Champion and IKC Centenary Winner titles. Cindy came to us from Lithuania, where she hails from a distinguished lineage of therapy dogs known for their exceptional calmness and strong character. She leaves an unforgettable impression on everyone she meets with her gentle and loving nature.</p>                        
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 mb-2">
+                        <div class="card">
+                            <img class="card-img-top dogFace" src={OurFamily2} alt="Card image cap" />
+                            <div class="card-body">
+                                <h5 class="card-title">Brita</h5>
+                                <p class="card-text">Meet Brita, the youngest member of our kennel. Despite her smaller size, Brita is true to the breed standard. She is also a show dog embodying elegance and poise in every aspect of her being. Brita possesses a calm and brave character, with a gentle and delicate temperament that endears her to all. She boasts a beautiful face expression that captures hearts wherever she goes.</p>                        
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-4 mb-2">
+                        <div class="card">
+                            <img class="card-img-top dogFace" src={OurFamily3}  alt="Card image cap" />
+                            <div class="card-body">
+                                <h5 class="card-title">Ollie</h5>
+                                <p class="card-text">Meet Ollie, our always happy stud dog! He's simply the best. He is the Junior Champion male, imported from Lithuania, with a big heart and playful spirit. Ollie is fully health tested, and his loving nature makes him a favourite among our family and friends. He is a proud daddy of our Litter A, passing on all of his best qualities to his beautiful puppies.<br /><br /></p>                        
+                            </div>
+                        </div> 
+                    </div>                   
+                </div>                
+            </div> 
+            <div class="col-12 text-center mt-3">                
+                <Link to="/ourdogs">
+                  <Button variant="success">Meet Us</Button>
+                </Link> 
+            </div>
         </div>
 
         <Footer />
